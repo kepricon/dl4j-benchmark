8414ec038d7bd1d0195927027a4feb42a95624a8
kepricon <kepricon@gmail.com>
2017-04-03 08:12:51 +0900

    fix model selector

--- dl4j-core-benchmark/src/main/java/org/deeplearning4j/models/ModelSelector.java
+++ dl4j-core-benchmark/src/main/java/org/deeplearning4j/models/ModelSelector.java
@@ -36,6 +36,7 @@ public class ModelSelector {
                 break;
             case INCEPTIONRESNETV1:
                 netmap.put(ModelType.INCEPTIONRESNETV1, new InceptionResNetV1(height, width, channels, numLabels, seed, iterations));
+                break;
             case FACENETNN4:
                 netmap.put(ModelType.FACENETNN4, new FaceNetNN4(height, width, channels, numLabels, seed, iterations));
                 break;
